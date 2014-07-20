b77bfddfa7c5dff5cad40a51e8fe426b3b559cd3
Clifton Regis <cregisexcel@yahoo.com>
2014-07-20 02:50:46 -0400

    associated and nested topics and posts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,12 +13,22 @@ require 'faker'
 end
 users = User.all
 
+15.times do
+  Topic.create(
+    name: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph
+    )
+end
+topics = Topic.all
+
+
  # Create Posts
  50.times do
    Post.create(
      title:  Faker::Lorem.sentence,
      body:   Faker::Lorem.paragraph,
-     user: users.sample
+     user: users.sample,
+     topic: topics.sample
     )
  end
  posts = Post.all
@@ -31,10 +41,33 @@ users = User.all
    )
  end
  
- User.first.update_attributes(
-  email: 'cregisexcel@yahoo.com',
-  password: 'helloworld'
-)
+ admin = User.new(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ admin.skip_confirmation!
+ admin.save
+ 
+ # Create a moderator
+ moderator = User.new(
+   name:     'Moderator User',
+   email:    'moderator@example.com', 
+   password: 'helloworld',
+   role:     'moderator'
+ )
+ moderator.skip_confirmation!
+ moderator.save
+ 
+ # Create a member
+ member = User.new(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld',
+ )
+ member.skip_confirmation!
+ member.save
 
 puts "Seed finished"
 puts "#{User.count} users created"
