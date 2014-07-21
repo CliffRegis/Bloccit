f123d7a3dfc51ea4156fb24835bc19bc9960fc41
Clifton Regis <cregisexcel@yahoo.com>
2014-07-21 02:24:46 -0400

    added simple form gemand replaced form_group tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,10 @@
 module ApplicationHelper
 
-  def simple_form_for(errors, &block)
+  def  simple_form_for (errors, &block)
     if errors.any?
         content_tag :div, capture(&block), class: 'form_group has-error'
     else
         content_tag :div, capture(&block), class: 'form_group'  
     end
-    
   end
 end
