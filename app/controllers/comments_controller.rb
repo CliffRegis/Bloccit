0fdc00ba9f4e77a5a543ebd64793eec7a6b7b624
Clifton Regis <cregisexcel@yahoo.com>
2014-08-15 12:42:34 -0400

    added ajax to create function for post/comment, created view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,11 +4,19 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
+    @comments = @post.comments
+    @comment.post = @post
+      @new_comment = Comment.new
     @comment.user_id = current_user.id 
+    authorize @comment
+
     if @comment.save
-      redirect_to [@post.topic, @post], notice: 'Comment was saved'
+      flash[:notice] = 'Comment was saved'
     else
-      redirect_to [@post.topic, @post], alert: 'Comment failed'
+      flash[:error] = 'Comment failed'
+    end
+    respond_with(@comment) do |format|
+      format.html { redirect_to [@post.topic, @post]}
     end
   end
 
@@ -23,10 +31,10 @@ class CommentsController < ApplicationController
       flash[:error] = "Comment was not removed."
     end
 
-  respond_with(@comment) do |format|
-    format.html { redirect_to[@post.topic, @post]}
+    respond_with(@comment) do |format|
+    format.html { redirect_to [@post.topic, @post]}
+    end
   end
-end
 
   def comment_params
     params.require(:comment).permit(:user_id, :id, :name, :post_id, :body)
